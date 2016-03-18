d0be8386472130f4e12dadb2f00d13b4ec4cb1ac
wanghe <wanghe@loginsight.cn>
2016-03-18 13:11:39 +0800

    IndentationError

--- setup.py
+++ setup.py
@@ -370,18 +370,18 @@ class BuildJavascriptCommand(Command):
             check_output(['git', 'submodule', 'init'], cwd=work_path)
             check_output(['git', 'submodule', 'update'], cwd=work_path)
 
-         log.info("running [npm install --quiet]")
-         check_output(['npm', 'install', '--quiet'], cwd=work_path)
+        log.info("running [npm install --quiet]")
+        check_output(['npm', 'install', '--quiet'], cwd=work_path)
 
-         By setting NODE_ENV=production, a few things happen
-           * React optimizes out certain code paths
-           * Webpack will add version strings to built/referenced assets
+        # By setting NODE_ENV=production, a few things happen
+        #   * React optimizes out certain code paths
+        #   * Webpack will add version strings to built/referenced assets
 
-         log.info("running [webpack]")
-         env = dict(os.environ)
-         env['SENTRY_STATIC_DIST_PATH'] = self.sentry_static_dist_path
-         env['NODE_ENV'] = 'production'
-         check_output(['node_modules/.bin/webpack', '-p', '--bail'],
+        log.info("running [webpack]")
+        env = dict(os.environ)
+        env['SENTRY_STATIC_DIST_PATH'] = self.sentry_static_dist_path
+        env['NODE_ENV'] = 'production'
+        check_output(['node_modules/.bin/webpack', '-p', '--bail'],
                       cwd=work_path, env=env)
 
     def _write_version_file(self, version_info):
